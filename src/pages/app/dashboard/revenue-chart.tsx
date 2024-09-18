ffe61cc4f1a2bccaf33d8d3f589353ca9f63cc2f
Victtor777 <victtor.eng.software@gmail.com>
2024-09-18 16:47:34 -0300

    Add loading aos gráficos

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -23,6 +23,7 @@ import { DateRangePicker } from "@/components/ui/date-range-picker";
 import { useMemo, useState } from "react";
 import { DateRange } from "react-day-picker";
 import { subDays } from "date-fns";
+import { Loader2 } from "lucide-react";
 
 // const data = [
 //   { date: "10/12", revenue: 1200 },
@@ -74,7 +75,7 @@ export function RevenueChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {chartData && (
+        {chartData ? (
           <ResponsiveContainer width={"100%"} height={240}>
             <LineChart data={chartData} style={{ fontSize: 12 }}>
               <YAxis
@@ -107,6 +108,10 @@ export function RevenueChart() {
               />
             </LineChart>
           </ResponsiveContainer>
+        ) : (
+          <div className="flex h-[240px] w-full items-center justify-center">
+            <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
+          </div>
         )}
       </CardContent>
     </Card>
