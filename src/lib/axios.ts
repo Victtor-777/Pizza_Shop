510b799a7da7423401cf47b985f619f9283c76d5
Victtor777 <victtor.eng.software@gmail.com>
2024-11-07 14:34:51 -0300

    Add mocks for order details and orders, and update MSW setup

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -8,10 +8,6 @@ export const api = axios.create({
 
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) =>
-      setTimeout(resolve, Math.round(Math.random() * 3000)),
-    );
-
     return config;
   });
 }
